10fb8eab83cab16fd0ba6300606f63f2692c99c6
Aman Raj <rajaman9355@gmail.com>
2024-09-24 23:50:06 +0530

    new add ons

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "../Navbar.jsx";
 function Posts() {
   return (
     <div>
-      posts
+      <div id="home" className="text-white w-full py-5 z-20">
+        <section className="mx-2 md:mx-10 h-screen md:h-auto md:pb-0 rounded-[30px] md:rounded-[50px] bg-[#0038c7] relative min-h-96">
+          <div className="relative">
+            <img
+              className="min-h-screen object- rounded-[30px] md:rounded-[50px] border-none w-full"
+              src="/gridd.png"
+              alt="background"
+            />
+          </div>
+          <div className="absolute inset-0 flex flex-col items-center justify-start text-center w-full"></div>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Posts
+export default Posts;
